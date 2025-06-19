22e686aab0520c9286619521550f6e8202624f01
currently-coding <165482223+currently-coding@users.noreply.github.com>
2025-06-19 09:43:33 +0200

    added bitboard printer for debugging

--- src/helper.rs
+++ src/helper.rs
@@ -1,5 +1,15 @@
 use crate::defs::{self, NrOf};
-
+pub fn print_bitboard(bitboard: u64) {
+    const LAST_BIT: u64 = 63;
+    for rank in 0..8 {
+        for file in (0..8).rev() {
+            let mask = 1u64 << (LAST_BIT - (rank * 8) - file);
+            let char = if bitboard & mask != 0 { '1' } else { '0' };
+            print!("{char} ");
+        }
+        println!();
+    }
+}
 pub fn get_bitmask(square: u8) -> u64 {
     if square >= NrOf::SQUARES as u8 {
         panic!(
